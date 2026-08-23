9e2e9a443ed20fc824952e3adc6db6e2d4922bdb
Daniel <danielrocha@estudante.ufsca.br>
2026-08-23 12:04:43 -0300

    script sql para criar tabela dos dados sujos

---
+++ sql/02_create_bronze_tables.sql
@@ -0,0 +1,31 @@
+-- criação da tabela com dados sujos na camada bronze
+CREATE TABLE bronze.transactions_raw (
+	transaction_id		BIGINT,
+	transaction_at		TIMESTAMPTZ(0),
+	transaction_amount	NUMERIC(15, 2),
+	transaction_status	VARCHAR(15),
+
+	customer_id			BIGINT,
+	customer_name		VARCHAR(100),
+	customer_email		VARCHAR(150),
+	customer_city		VARCHAR(50),
+	customer_state		VARCHAR(50),
+	customer_region		VARCHAR(30),
+	customer_status		VARCHAR(30),
+
+	merchant_id			BIGINT,
+	merchant_name		VARCHAR(100),
+	merchant_category	VARCHAR(100),
+	merchant_city		VARCHAR(50),
+	merchant_state		VARCHAR(50),
+	merchant_region		VARCHAR(30),
+	merchant_status		VARCHAR(30),
+
+	payment_method_id		BIGINT,
+	payment_method_name		VARCHAR(30),
+	payment_method_type		VARCHAR(15),
+	payment_method_brand	VARCHAR(30),
+
+	source_file		VARCHAR(100),
+	ingested_at		TIMESTAMPTZ(0) DEFAULT CURRENT_TIMESTAMP
+);
