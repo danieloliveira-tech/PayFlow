3dcb7d4ce871e4580fa031e3ea1e9582dad69731
Daniel <danielrocha@estudante.ufsca.br>
2026-08-24 16:43:16 -0300

    Incrementabilidade aplicada a cama silver funcional

--- sql/03_create_pipeline_tables.sql
+++ sql/03_create_pipeline_tables.sql
@@ -3,3 +3,8 @@ CREATE TABLE pipeline.processed_files (
 	processed_at	TIMESTAMPTZ(0) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	row_count		BIGINT NOT NULL
 );
+
+CREATE TABLE pipeline.silver_processed_files (
+    file_name       VARCHAR(100) PRIMARY KEY,
+    processed_at    TIMESTAMPTZ(0) NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
